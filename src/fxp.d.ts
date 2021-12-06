b0e1e39bb752cd1db5779c4205c670731bcfef62
zhangyu1818 <723156735@qq.com>
2021-12-06 14:47:04 +0800

    fix React Native error

--- src/fxp.d.ts
+++ src/fxp.d.ts
@@ -33,35 +33,12 @@ type validationOptions = {
 };
 type validationOptionsOptional = Partial<validationOptions>;
 
-type XmlBuilderOptions = {
-  attributeNamePrefix: string;
-  attributesGroupName: false | string;
-  textNodeName: string;
-  ignoreAttributes: boolean;
-  cdataPropName: false | string;
-  commentPropName: false | string;
-  format: boolean;
-  indentBy: string;
-  arrayNodeName: string;
-  suppressEmptyNode: boolean;
-  suppressBooleanAttributes: boolean;
-  preserveOrder: boolean;
-  unpairedTags: string[];
-  stopNodes: string[];
-  tagValueProcessor: (name: string, value: string) => string;
-  attributeValueProcessor: (name: string, value: string) => string;
-  processEntities: boolean;
-};
-type XmlBuilderOptionsOptional = Partial<XmlBuilderOptions>;
-
-type ESchema = string | object | Array<string|object>;
-
 type ValidationError = {
-  err: { 
+  err: {
     code: string;
     msg: string,
     line: number,
-    col: number 
+    col: number
   };
 };
 
@@ -78,8 +55,4 @@ export class XMLParser {
 
 export class XMLValidator{
   static validate(  xmlData: string,  options?: validationOptionsOptional): true | ValidationError;
-}
-export class XMLBuilder {
-  constructor(options: XmlBuilderOptionsOptional);
-  build(jObj: any): any;
 }
--- src/fxp.js
+++ src/fxp.js
@@ -2,10 +2,8 @@
 
 const validator = require('./validator');
 const XMLParser = require('./xmlparser/XMLParser');
-const XMLBuilder = require('./xmlbuilder/json2xml');
 
 module.exports = {
   XMLParser: XMLParser,
   XMLValidator: validator,
-  XMLBuilder: XMLBuilder
 }
